4d3036324be39c885be7e3343fbf776ee8c673f1
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2023-03-05 10:44:34 -0600

    Fixed geocoding request url

--- lib/geocoding.ts
+++ lib/geocoding.ts
@@ -4,7 +4,7 @@ import { Location } from "@/interfaces/location";
 export const getCity = async (location: Location) => {
   const apiKey = process.env.API_KEY;
   const res = await fetch(
-    `http://api.openweathermap.org/geo/1.0/reverse?lat=${location.lat}&lon=${location.lon}&limit=1&appid=${apiKey}`
+    `https://api.openweathermap.org/geo/1.0/reverse?lat=${location.lat}&lon=${location.lon}&limit=1&appid=${apiKey}`
   );
   const data: GeocodeData[] = await res.json();
   return data[0].name;
